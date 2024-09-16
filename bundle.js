3c913cf7e032d67cc3222d25f4e4109120b9ebad
Joel Berg <bergjoel93@gmail.com>
2024-09-16 08:46:36 -0600

    Fixed the transaction addition so it adds new transaction to top of container instead of bottom.

--- bundle.js
+++ bundle.js
@@ -6125,7 +6125,7 @@ class AddTransaction {
         //console.log(card);
         //console.log(`${choice} clicked`);
         if (card) {
-          transactionContainer.appendChild(card);
+          transactionContainer.insertAdjacentElement('afterbegin', card);
           // increase count on option
           this.options[choice].count++;
           this.buttonsContainer.innerHTML = ``;
